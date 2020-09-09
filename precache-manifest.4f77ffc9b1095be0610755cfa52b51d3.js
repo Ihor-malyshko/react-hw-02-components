b2bbb25fbad8e54d9be873deead70463085e1cd7
ihor <malishkoio@gmail.com>
2020-09-09 22:54:30 +0300

    Updates

---
+++ precache-manifest.4f77ffc9b1095be0610755cfa52b51d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34efbe04ac2eae421fd07b0f23e4a74",
+    "url": "/react-hw-02-components/index.html"
+  },
+  {
+    "revision": "af4c6b13402fa4630869",
+    "url": "/react-hw-02-components/static/js/2.3e7dfad4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-components/static/js/2.3e7dfad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c08b4ff89d0e2f6cd8",
+    "url": "/react-hw-02-components/static/js/main.bf1f0117.chunk.js"
+  },
+  {
+    "revision": "db465968d435074ec27a",
+    "url": "/react-hw-02-components/static/js/runtime-main.925d8b28.js"
+  }
+]);
---
+++ static/js/main.bf1f0117.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-hw-02-components"]=this["webpackJsonpreact-hw-02-components"]||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2);var o=function(){return a.a.createElement(a.a.Fragment,null,a.a.createElement("h2",null,"react-hw-02-components"))};t.n(c).a.render(a.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.bf1f0117.chunk.js.map
